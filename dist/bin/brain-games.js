969335d165994b83958a5a20c6424d2d39e200d1
marksaponenko <forpokerstars.ru@yandex.ru>
2017-05-16 15:40:11 +0300

    change version

--- dist/bin/brain-games.js
+++ dist/bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-'use strict';
 
-var _ = require('../');
+// import { printUsername } from '../';
 
-(0, _.printUsername)();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFDQTs7QUFFQSIsImZpbGUiOiJicmFpbi1nYW1lcy5qcyIsInNvdXJjZXNDb250ZW50IjpbIlxuaW1wb3J0IHsgcHJpbnRVc2VybmFtZSB9IGZyb20gJy4uLyc7XG5cbnByaW50VXNlcm5hbWUoKTtcbiJdfQ==
+// printUsername();
+"use strict";
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUNBOztBQUVBIiwiZmlsZSI6ImJyYWluLWdhbWVzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiXG4vLyBpbXBvcnQgeyBwcmludFVzZXJuYW1lIH0gZnJvbSAnLi4vJztcblxuLy8gcHJpbnRVc2VybmFtZSgpO1xuIl19
