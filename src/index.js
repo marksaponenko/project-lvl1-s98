50781e79f6b3ef26550b910e360276793dc77019
marksaponenko <forpokerstars.ru@yandex.ru>
2017-05-17 19:05:29 +0300

    Revert "Revert "Revert "a"""
    This reverts commit 4fcfd4c7a6fe4a0c8cfb72d1c1bb80522beda5d6.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
 import readlineSync from 'readline-sync';
-//import { getRandomExpression, getCorrectAnswer } from './games/brain-calc';
+import { getRandomExpression, getCorrectAnswer } from './games/brain-calc';
 
 console.log('Welcome to the Brain Games!');
 
 console.log('What is the result of the expression?');
 
+const randomExpression = getRandomExpression();
+const correctAnswer = getCorrectAnswer();
 const userName = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${userName}!`);
-const getRandomExpression = () => Math.floor(Math.random()*100);
-const randomExpression = getRandomExpression()
-//const correctAnswer = getCorrectAnswer();
 let numberOfTries = 1;
-
 const askQuestion = () => {
   if (numberOfTries > 3) {
     return console.log(`Congratulations, ${userName}!`);
   }
   console.log(`Question: ${randomExpression}`);
   const userAnswer = readlineSync.question('Your answer: ');
-  const correctAnswer = randomExpression % 2 === 0 ? 'yes' : 'no'
-  if (userAnswer === correctAnswer) {
+  if (correctAnswer == userAnswer) {
     numberOfTries += 1;
     console.log('Correct!');
     return askQuestion();
