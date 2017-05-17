72d5ab409ee3137c983b564bf7b44376f6452fba
marksaponenko <forpokerstars.ru@yandex.ru>
2017-05-17 19:04:08 +0300

    a

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import readlineSync from 'readline-sync';
-import { getRandomExpression, getCorrectAnswer } from './games/brain-calc';
+//import { getRandomExpression, getCorrectAnswer } from './games/brain-calc';
 
 console.log('Welcome to the Brain Games!');
 
 console.log('What is the result of the expression?');
 
-const randomExpression = getRandomExpression();
-const correctAnswer = getCorrectAnswer();
 const userName = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${userName}!`);
+const getRandomExpression = () => Math.floor(Math.random()*100);
+const randomExpression = getRandomExpression()
+//const correctAnswer = getCorrectAnswer();
 let numberOfTries = 1;
+
 const askQuestion = () => {
   if (numberOfTries > 3) {
     return console.log(`Congratulations, ${userName}!`);
   }
   console.log(`Question: ${randomExpression}`);
   const userAnswer = readlineSync.question('Your answer: ');
-  if (correctAnswer == userAnswer) {
+  const correctAnswer = randomExpression % 2 === 0 ? 'yes' : 'no'
+  if (userAnswer === correctAnswer) {
     numberOfTries += 1;
     console.log('Correct!');
     return askQuestion();
