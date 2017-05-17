2237db3be4789aba79d0fc3614f5d252b85d8308
marksaponenko <forpokerstars.ru@yandex.ru>
2017-05-17 20:58:27 +0300

    change games architecture

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,26 +1,30 @@
-//const getFirstOperand = () => ;
-//const getSecondOperand = () => Math.floor(Math.random() * 25);
-//const firstOperand = getFirstOperand();
-//const secondOperand = getSecondOperand();
 
-export const getRandomExpression = (firstOperand, secondOperand) => {
+
+export const getRandomNumber = () => {
+  const getFirstOperand = () => Math.floor(Math.random() * 25);
+  const getSecondOperand = () => Math.floor(Math.random() * 25);
+
+  const firstOperand = getFirstOperand();
+  const secondOperand = getSecondOperand();
+
   const mult = `${firstOperand} * ${secondOperand}`;
   const add = `${firstOperand} + ${secondOperand}`;
   const subtr = `${firstOperand} + ${secondOperand}`;
+
   const arrOfOperations = [mult, add, subtr];
-  return arrOfOperations[Math.floor(Math.random() * 3)];
-}
-getRandomExpression(Math.floor(Math.random() * 25), Math.floor(Math.random() * 25))
+  const operation = arrOfOperations[Math.floor(Math.random() * 3)];
 
+  return operation;
+};
 
+export const getCorrectAnswer = (operation) => {
+  const strToArr = operation.split(' ');
 
-
-//export const getCorrectAnswer = (firstOperand, secondOperand) => {
-  //if (result[2] || result[3] === '*') {
-    //return firstOperand * secondOperand;
-  //}
-  //else if (result[2] || result[3] === '+') {
-    //return firstOperand + secondOperand;
-  //}
-  //return firstOperand - secondOperand;
-//}
+  if (strToArr[1] === '*') {
+    return Number(strToArr[0]) * Number(strToArr[2]);
+  }
+  else if (strToArr[1] === '+') {
+    return Number(strToArr[0]) + Number(strToArr[2]);
+  }
+  return Number(strToArr[0]) - Number(strToArr[2]);
+};
--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
 import readlineSync from 'readline-sync';
-//import { getRandomExpression, getCorrectAnswer } from './games/brain-calc';
+import { getRandomNumber, getCorrectAnswer } from './games/brain-calc';
 
 console.log('Welcome to the Brain Games!');
-
 console.log('What is the result of the expression?');
 
 const userName = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${userName}!`);
-const getRandomExpression = () => Math.floor(Math.random()*100);
-const randomExpression = getRandomExpression()
-//const correctAnswer = getCorrectAnswer();
+
 let numberOfTries = 1;
 
 const askQuestion = () => {
   if (numberOfTries > 3) {
     return console.log(`Congratulations, ${userName}!`);
   }
-  console.log(`Question: ${randomExpression}`);
+
+  const randomNumber = getRandomNumber();
+  const correctAnswer = getCorrectAnswer(randomNumber);
+
+  console.log(`Question: ${randomNumber}`);
   const userAnswer = readlineSync.question('Your answer: ');
-  const correctAnswer = randomExpression % 2 === 0 ? 'yes' : 'no'
-  if (userAnswer === correctAnswer) {
+
+  if (correctAnswer == userAnswer) {
     numberOfTries += 1;
     console.log('Correct!');
     return askQuestion();
   }
-  return console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLets try again, ${userName}`);
+  return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLets try again, ${userName}`);
 };
 
 export default askQuestion;
