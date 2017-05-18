04e01ac228622e4ccf9b2c3b596df1bf3ca0ceb1
marksaponenko <forpokerstars.ru@yandex.ru>
2017-05-18 10:33:37 +0300

    Remove the now ignored directory "dist"

--- dist/bin/brain-games.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-// import { printUsername } from '../';
-
-// printUsername();
-"use strict";
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUNBOztBQUVBIiwiZmlsZSI6ImJyYWluLWdhbWVzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiXG4vLyBpbXBvcnQgeyBwcmludFVzZXJuYW1lIH0gZnJvbSAnLi4vJztcblxuLy8gcHJpbnRVc2VybmFtZSgpO1xuIl19
