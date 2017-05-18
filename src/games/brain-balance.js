58283a035b3b6bfbf4eb1bb835db1751f3999b73
marksaponenko <forpokerstars.ru@yandex.ru>
2017-05-18 17:32:42 +0300

    debug smth

--- src/games/brain-balance.js
+++ src/games/brain-balance.js
@@ -1,26 +1,25 @@
 import askQuestion from '..';
 
-export const greetingMessage = 'Balance the given number.'
+export const greetingMessage = 'Balance the given number.';
 
-const getRandomNumber = () => Math.floor(Math.random() * (9999 - 100 + 1)) + 100;
+const getRandomNumber = () => Math.floor(Math.random() * 1000);
 
 const balanceNumber = (arrOfNumbers) => {
-
   const arrLength = arrOfNumbers.length;
   const sortedArr = arrOfNumbers.sort();
 
   if ((sortedArr[arrLength - 1] - sortedArr[0]) > 1) {
     sortedArr[arrLength - 1] -= 1;
-    sortedArr[0] = + sortedArr[0] + 1;
+    sortedArr[0] = +sortedArr[0] + 1;
 
     return balanceNumber(sortedArr);
   }
-  return arrOfNumbers.join().replace(/\,/g, "")
+  return arrOfNumbers.join().replace(/\,/g, '');
 };
 
 const getBalancedNumber = (number) => {
   const arrOfNumbers = String(number).split('');
   return balanceNumber(arrOfNumbers);
-}
+};
 
-export const startGame = () => askQuestion(getRandomNumber, getBalancedNumber)
+export const startGame = () => askQuestion(getRandomNumber, getBalancedNumber);
